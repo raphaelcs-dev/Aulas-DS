08797740edf460780d553d03ded67878fd46df43
unknown <RAPHAEL_5202@UIRAPURU.BR>
2026-03-09 15:58:36 -0300

    coomeçando

---
+++ projeto-01/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main{
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+
+    }
+
+}
