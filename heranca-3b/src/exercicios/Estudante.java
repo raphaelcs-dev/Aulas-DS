fe7d060ae9366d404df54193060c9dd839cce95c
unknown <RAPHAEL_5202@UIRAPURU.BR>
2026-08-24 15:58:03 -0300

    Fazendo um exercício de herança

---
+++ heranca-3b/src/exercicios/Estudante.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+public class Estudante extends Pessoa {
+    private int matricula;
+    public int getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+}
+
+
---
+++ heranca-3b/src/exercicios/TestaPessoa.java
@@ -0,0 +1,17 @@
+package exercicios;
+
+public class TestaPessoa {
+    public static void main(String[]args){
+
+        Estudante estudante = new Estudante();
+
+        estudante.setNome("Raphael");
+        estudante.setIdade(16);
+        estudante.setMatricula(2026001);
+        System.out.println("Nome: " + estudante.getNome());
+        System.out.println("Idade: " + estudante.getIdade());
+        System.out.println("Matrícula: " + estudante.getMatricula());
+    }
+
+}
+
